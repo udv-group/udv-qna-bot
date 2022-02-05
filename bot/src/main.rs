db6eab05968bf1c8b6fd05ea4e1be1efb673809f
mikhailantoshkin <mike.antoshkin@gmail.com>
2022-02-05 15:36:45 +0500

    Implement keyboard state change

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -2,13 +2,13 @@ use std::error::Error;
 use teloxide::{
     payloads::SendMessageSetters,
     prelude::*,
-    types::{
-        InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputMessageContent,
-        InputMessageContentText,
-    },
+    types::{InlineKeyboardButton, InlineKeyboardMarkup},
     utils::command::BotCommand,
 };
 
+use serde::{Deserialize, Serialize};
+
+use serde_repr::{Deserialize_repr, Serialize_repr};
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
 #[derive(BotCommand)]
@@ -20,22 +20,63 @@ enum Command {
     Start,
 }
 
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Clone, Copy)]
+#[repr(u8)]
+enum KeyboardType {
+    Question = 1,
+    Category = 2,
+    Answer = 3,
+}
+
+#[derive(Serialize_repr, Deserialize_repr)]
+#[repr(u8)]
+enum StateSwitch {
+    Next = 1,
+    Back = 2,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CallbackInfo {
+    // id of the element being displayed
+    button_id: i32,
+    // current state of the keyboard on user's screen
+    keyboard_type: KeyboardType,
+    // where to move from here
+    state: StateSwitch,
+}
+
 /// Creates a keyboard made by buttons in a big column.
-fn make_keyboard() -> InlineKeyboardMarkup {
+fn make_keyboard(button_text: Vec<String>, keyboard_type: KeyboardType) -> InlineKeyboardMarkup {
     let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
 
-    let debian_versions = [
-        "Buzz", "Rex", "Bo", "Hamm", "Slink", "Potato", "Woody", "Sarge", "Etch", "Lenny",
-        "Squeeze", "Wheezy", "Jessie", "Stretch", "Buster", "Bullseye",
-    ];
-
-    for versions in debian_versions.chunks(3) {
-        let row = versions
+    for text in button_text.chunks(3) {
+        let row = text
             .iter()
-            .map(|&version| InlineKeyboardButton::callback(version.to_owned(), version.to_owned()))
+            .map(|text| {
+                let callback_info = CallbackInfo {
+                    button_id: text.len() as i32, // placeholder, going to be the id of the DB element
+                    keyboard_type,
+                    state: StateSwitch::Next,
+                };
+                InlineKeyboardButton::callback(
+                    text.to_owned(),
+                    serde_json::to_string(&callback_info).unwrap(),
+                )
+            })
             .collect();
 
         keyboard.push(row);
+    }
+    if (keyboard_type == KeyboardType::Answer) | (keyboard_type == KeyboardType::Question) {
+        let callback_info = CallbackInfo {
+            button_id: 42,
+            keyboard_type,
+            state: StateSwitch::Back,
+        };
+        keyboard.push(vec![InlineKeyboardButton::callback(
+            "Go Back".to_string(),
+            serde_json::to_string(&callback_info).unwrap(),
+        )]);
     }
 
     InlineKeyboardMarkup::new(keyboard)
@@ -55,8 +96,11 @@ async fn message_handler(
             }
             Ok(Command::Start) => {
                 // Create a list of buttons and send them.
-                let keyboard = make_keyboard();
-                cx.answer("Debian versions:").reply_markup(keyboard).await?;
+                let words = vec!["Foo1".to_owned(), "Bar2".to_owned()];
+                let keyboard = make_keyboard(words, KeyboardType::Category);
+                cx.answer("Available categories:")
+                    .reply_markup(keyboard)
+                    .await?;
             }
 
             Err(_) => {
@@ -68,20 +112,22 @@ async fn message_handler(
     Ok(())
 }
 
-async fn inline_query_handler(
-    cx: UpdateWithCx<AutoSend<Bot>, InlineQuery>,
+async fn update_message<T: Into<String>>(
+    bot: AutoSend<Bot>,
+    message: Option<Message>,
+    text: T,
+    keyboard: InlineKeyboardMarkup,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let UpdateWithCx { requester: bot, update: query } = cx;
-
-    let choose_debian_version = InlineQueryResultArticle::new(
-        "0",
-        "Chose debian version",
-        InputMessageContent::Text(InputMessageContentText::new("Debian versions:")),
-    )
-    .reply_markup(make_keyboard());
-
-    bot.answer_inline_query(query.id, vec![choose_debian_version.into()]).await?;
-
+    match message {
+        Some(Message { id, chat, .. }) => {
+            bot.edit_message_text(chat.id, id, text)
+                .reply_markup(keyboard)
+                .await?;
+        }
+        None => {
+            log::warn!("No message, wtf");
+        }
+    }
     Ok(())
 }
 
@@ -90,33 +136,41 @@ async fn inline_query_handler(
 async fn callback_handler(
     cx: UpdateWithCx<AutoSend<Bot>, CallbackQuery>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let UpdateWithCx { requester: bot, update: query } = cx;
-
-    if let Some(version) = query.data {
-        let text = format!("You chose: {}", version);
-
-        match query.message {
-            Some(Message { id, chat, .. }) => {
-                bot.edit_message_text(chat.id, id, text).await?;
+    let UpdateWithCx {
+        requester: bot,
+        update: query,
+    } = cx;
+    if let Some(data) = query.data {
+        log::info!("Selected: {}", data);
+        let callback_info: CallbackInfo = serde_json::from_str(&data)?;
+        match callback_info.keyboard_type {
+            KeyboardType::Category | KeyboardType::Answer => {
+                let keyboard = make_keyboard(
+                    vec!["Question1".to_owned(), "Question2".to_owned()],
+                    KeyboardType::Question,
+                );
+                update_message(bot, query.message, "Select a question", keyboard).await?;
             }
-            None => {
-                if let Some(id) = query.inline_message_id {
-                    bot.edit_message_text_inline(dbg!(id), text).await?;
+            KeyboardType::Question => match callback_info.state {
+                StateSwitch::Next => {
+                    let keyboard = make_keyboard(vec![], KeyboardType::Answer);
+                    update_message(bot, query.message, "Some answer", keyboard).await?;
                 }
-            }
+                StateSwitch::Back => {
+                    let keyboard = make_keyboard(
+                        vec!["Foo1".to_owned(), "Bar2".to_owned()],
+                        KeyboardType::Category,
+                    );
+                    update_message(bot, query.message, "Available categories", keyboard).await?;
+                }
+            },
         }
-
-        log::info!("You chose: {}", version);
-    }
+    };
 
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    teloxide::enable_logging!();
-    log::info!("Starting bot...");
-
+async fn run() -> Result<(), Box<dyn Error>> {
     let bot = Bot::from_env().auto_send();
 
     Dispatcher::new(bot)
@@ -130,15 +184,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 callback_handler(cx).await.log_on_error().await;
             })
         })
-        .inline_queries_handler(|rx: DispatcherHandlerRx<AutoSend<Bot>, InlineQuery>| {
-            UnboundedReceiverStream::new(rx).for_each_concurrent(None, |cx| async move {
-                inline_query_handler(cx).await.log_on_error().await;
-            })
-        })
         .dispatch()
         .await;
 
-    log::info!("Closing bot... Goodbye!");
+    Ok(())
+}
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    teloxide::enable_logging!();
+    log::info!("Starting bot...");
+    run().await?;
+    log::info!("Closing bot... Goodbye!");
     Ok(())
 }

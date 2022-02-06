55de53e6409730bf40c7827d91ecf3c7d22a7bde
mikhailantoshkin <mike.antoshkin@gmail.com>
2022-02-06 23:39:19 +0500

    Hacked bot to work with sqlite

--- README.md
+++ README.md
@@ -3,17 +3,36 @@ Q&A bot for internal use in UDV
 
 ## Bot
 ### Development
-1. Get yourself a token from [@Botfather](https://t.me/botfather). It looks something like `123456789:qweqweqwe`
-2. Set up the environmental variable `TELOXIDE_TOKEN`
+_This is aimed at *NIX users, if you are using Windows ~~tough luck~~ lookup similar commands_
+
+Install sqlite3 build dependencies and sqlite3 CLI
 ```
-# Unix-like
+# For Debian
+$ sudo apt-get install libsqlite3-dev sqlite
+```
+Create SQLite database and set the environmental variable `DATABASE_URL`
+```
+$ touch <db file path>
+$ export DATABASE_URL=file:<db file path>
+```
+Install `diesel_cli` and run database migrations
+```
+$ cargo install diesel_cli --no-default-features --features sqlite
+$ diesel migration run
+```
+Add some data to the database
+```
+$ sqlite <db file path>
+sqlite> insert into categories(name) values("test1");
+sqlite> insert into categories(name) values("test2");
+sqlite> insert into questions(question, answer, category) values("funny question1", "funny an
+swer1", 1);
+sqlite> insert into questions(question, answer, category) values("funny question2", "funny an
+swer2", 2);
+```
+Get yourself a token from [@Botfather](https://t.me/botfather). It looks something like `123456789:qweqweqwe`. Then set up the environmental variable `TELOXIDE_TOKEN`
+```
 $ export TELOXIDE_TOKEN=<Your token here>
-
-# Windows command line
-$ set TELOXIDE_TOKEN=<Your token here>
-
-# Windows PowerShell
-$ $env:TELOXIDE_TOKEN=<Your token here>
 ```
 To start the bot run
 ```
--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,4 +1,10 @@
+#[macro_use]
+use diesel;
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+
 use std::error::Error;
+use std::sync::Arc;
 use teloxide::{
     payloads::SendMessageSetters,
     prelude::*,
@@ -8,7 +14,11 @@ use teloxide::{
 
 use serde::{Deserialize, Serialize};
 
+use db;
+use db::models::Category;
+use db::schema::questions::category;
 use serde_repr::{Deserialize_repr, Serialize_repr};
+use tokio::sync::Mutex;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
 #[derive(BotCommand)]
@@ -38,25 +48,37 @@ enum StateSwitch {
 #[derive(Serialize, Deserialize)]
 struct CallbackInfo {
     // id of the element being displayed
+    #[serde(rename = "i")]
     button_id: i32,
     // current state of the keyboard on user's screen
+    #[serde(rename = "kt")]
     keyboard_type: KeyboardType,
     // where to move from here
+    #[serde(rename = "s")]
     state: StateSwitch,
+    // ???
+    #[serde(rename = "psi")]
+    previous_state_id: Option<i32>,
 }
 
+//todo: this api is garbage, fix it
 /// Creates a keyboard made by buttons in a big column.
-fn make_keyboard(button_text: Vec<String>, keyboard_type: KeyboardType) -> InlineKeyboardMarkup {
+fn make_keyboard(
+    button_info: Vec<(String, i32)>,
+    current_keyboard_id: Option<i32>,
+    keyboard_type: KeyboardType,
+) -> InlineKeyboardMarkup {
     let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
 
-    for text in button_text.chunks(3) {
+    for text in button_info.chunks(3) {
         let row = text
             .iter()
-            .map(|text| {
+            .map(|(text, id)| {
                 let callback_info = CallbackInfo {
-                    button_id: text.len() as i32, // placeholder, going to be the id of the DB element
+                    button_id: *id,
                     keyboard_type,
                     state: StateSwitch::Next,
+                    previous_state_id: current_keyboard_id,
                 };
                 InlineKeyboardButton::callback(
                     text.to_owned(),
@@ -72,6 +94,7 @@ fn make_keyboard(button_text: Vec<String>, keyboard_type: KeyboardType) -> Inlin
             button_id: 42,
             keyboard_type,
             state: StateSwitch::Back,
+            previous_state_id: current_keyboard_id,
         };
         keyboard.push(vec![InlineKeyboardButton::callback(
             "Go Back".to_string(),
@@ -87,7 +110,10 @@ fn make_keyboard(button_text: Vec<String>, keyboard_type: KeyboardType) -> Inlin
 /// markup with the `InlineKeyboardMarkup`.
 async fn message_handler(
     cx: UpdateWithCx<AutoSend<Bot>, Message>,
+    conn: SqliteConnection,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
+    use db::schema::categories::dsl::*;
+
     if let Some(text) = cx.update.text() {
         match BotCommand::parse(text, "buttons") {
             Ok(Command::Help) => {
@@ -96,8 +122,11 @@ async fn message_handler(
             }
             Ok(Command::Start) => {
                 // Create a list of buttons and send them.
-                let words = vec!["Foo1".to_owned(), "Bar2".to_owned()];
-                let keyboard = make_keyboard(words, KeyboardType::Category);
+                let results: Vec<(String, i32)> = db::get_categories(&conn)
+                    .into_iter()
+                    .map(|category_| (category_.name, category_.id))
+                    .collect();
+                let keyboard = make_keyboard(results, None, KeyboardType::Category);
                 cx.answer("Available categories:")
                     .reply_markup(keyboard)
                     .await?;
@@ -135,6 +164,7 @@ async fn update_message<T: Into<String>>(
 /// those buttons writing a text with the selected Debian version.
 async fn callback_handler(
     cx: UpdateWithCx<AutoSend<Bot>, CallbackQuery>,
+    conn: SqliteConnection,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     let UpdateWithCx {
         requester: bot,
@@ -144,23 +174,47 @@ async fn callback_handler(
         log::info!("Selected: {}", data);
         let callback_info: CallbackInfo = serde_json::from_str(&data)?;
         match callback_info.keyboard_type {
-            KeyboardType::Category | KeyboardType::Answer => {
+            // if user tapped category button switch to questions in that category
+            // user can only go Next
+            KeyboardType::Category => {
+                let results: Vec<(String, i32)> =
+                    db::get_questions_by_category(&conn, callback_info.button_id) // todo: filter by category
+                        .into_iter()
+                        .map(|item| (item.question, item.id))
+                        .collect();
                 let keyboard = make_keyboard(
-                    vec!["Question1".to_owned(), "Question2".to_owned()],
+                    results,
+                    Some(callback_info.button_id),
                     KeyboardType::Question,
                 );
                 update_message(bot, query.message, "Select a question", keyboard).await?;
             }
+            // if user tapped "Go Back" on the answer - return to the questions list
+            // user can only go Back
+            KeyboardType::Answer => {
+                let results: Vec<(String, i32)> =
+                    db::get_questions_by_category(&conn, callback_info.previous_state_id.unwrap()) // todo: filter by category
+                        .into_iter()
+                        .map(|item| (item.question, item.id))
+                        .collect();
+                let keyboard = make_keyboard(results, None, KeyboardType::Question);
+                update_message(bot, query.message, "Select a question", keyboard).await?;
+            }
+            // if user tapped something on questions keyboard
             KeyboardType::Question => match callback_info.state {
+                // user selected a question, show the answer, set the Go Back button id as this question id
                 StateSwitch::Next => {
-                    let keyboard = make_keyboard(vec![], KeyboardType::Answer);
-                    update_message(bot, query.message, "Some answer", keyboard).await?;
+                    let question = db::get_question_by_id(&conn, callback_info.button_id);
+                    let keyboard = make_keyboard(vec![], question.category, KeyboardType::Answer);
+                    update_message(bot, query.message, question.answer, keyboard).await?;
                 }
+                // user wants to go back to category selection
                 StateSwitch::Back => {
-                    let keyboard = make_keyboard(
-                        vec!["Foo1".to_owned(), "Bar2".to_owned()],
-                        KeyboardType::Category,
-                    );
+                    let results: Vec<(String, i32)> = db::get_categories(&conn)
+                        .into_iter()
+                        .map(|category_| (category_.name, category_.id))
+                        .collect();
+                    let keyboard = make_keyboard(results, None, KeyboardType::Category);
                     update_message(bot, query.message, "Available categories", keyboard).await?;
                 }
             },
@@ -172,16 +226,22 @@ async fn callback_handler(
 
 async fn run() -> Result<(), Box<dyn Error>> {
     let bot = Bot::from_env().auto_send();
-
+    // TODO: share the connection somehow
     Dispatcher::new(bot)
         .messages_handler(|rx: DispatcherHandlerRx<AutoSend<Bot>, Message>| {
             UnboundedReceiverStream::new(rx).for_each_concurrent(None, |cx| async move {
-                message_handler(cx).await.log_on_error().await;
+                message_handler(cx, db::establish_connection().unwrap())
+                    .await
+                    .log_on_error()
+                    .await;
             })
         })
         .callback_queries_handler(|rx: DispatcherHandlerRx<AutoSend<Bot>, CallbackQuery>| {
             UnboundedReceiverStream::new(rx).for_each_concurrent(None, |cx| async move {
-                callback_handler(cx).await.log_on_error().await;
+                callback_handler(cx, db::establish_connection().unwrap())
+                    .await
+                    .log_on_error()
+                    .await;
             })
         })
         .dispatch()

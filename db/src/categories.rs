a04e5a915daf841243ccbb7f1a2a5ca030b066ec
mikhailantoshkin <mike.antoshkin@gmail.com>
2023-09-03 22:46:01 +0900

    make reordering work

--- db/src/categories.rs
+++ db/src/categories.rs
@@ -10,6 +10,11 @@ pub struct Category {
     pub ordering: i64,
 }
 
+pub struct CategoryReorder {
+    pub id: i64,
+    pub ordering: i64,
+}
+
 pub async fn get_category(pool: &SqlitePool, id: i64) -> sqlx::Result<Category> {
     sqlx::query_as!(
         Category,
@@ -102,7 +107,10 @@ pub async fn delete_category(pool: &SqlitePool, category_id: i64) -> sqlx::Resul
     Ok(())
 }
 
-pub async fn reorder_categories(pool: &SqlitePool, categories: Vec<Category>) -> sqlx::Result<()> {
+pub async fn reorder_categories(
+    pool: &SqlitePool,
+    categories: Vec<CategoryReorder>,
+) -> sqlx::Result<()> {
     let mut transaction = pool.begin().await?;
     for category in categories {
         sqlx::query!(
